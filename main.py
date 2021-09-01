ae96ccf0d04edff25d1fd253ad40e63c185c35a7
Meesha Chauhan <meeshachauhan@Meeshas-MacBook-Air.local>
2021-08-31 21:02:56 -0700

    changes

--- main.py
+++ main.py
@@ -9,22 +9,22 @@ def findMaxRange(intervalsArray, n):
 
     # get the min range that when removed will give max value
     for i in range(n):
-        var1 = sortedArr[i][0] if (i < n-1) else 10000000
-        var2 = sortedArr[i][1] if (i>0) else -1
+        var1 = sortedArr[i+1][0] if (i < n-1) else 10000000
+        var2 = sortedArr[i-1][1] if (i>0) else -1
         impact = min(sortedArr[i][1], var1) - max(sortedArr[i][0], var2)
         if impact < minValue :
             minValue = impact
     
     # get max contiguous intervals 
-    for i in range(n):
-        if sortedArr[index][1] >= sortedArr[i][1]:
+    for i in range(1,n,1):
+        if sortedArr[index][1] >= sortedArr[i][0]:
             sortedArr[index][1] = max(sortedArr[index][1], sortedArr[i][1])
             sortedArr[index][0] = min(sortedArr[index][0], sortedArr[i][0])
         else:
             index+=1
             sortedArr[index] = sortedArr[i]  
     # Total range of maximum contiguous intervals
-    for i in range(index):
+    for i in range(index+1):
         total = total + sortedArr[i][1] - sortedArr[i][0]
     
     # Return the difference of total range and minimum impact value
